28c613e4234e5b51e728c4ac17b940ad5377d21a
Krish Sharma <krishsharma3355@gmail.com>
2026-09-14 21:09:35 +0530

    scoping

---
+++ scoping/index.js
@@ -0,0 +1,22 @@
+// function a() {
+//   console.log(b);
+// }
+// let b = 10;
+// a()
+
+
+// function c(){
+//     d()
+//     function d(){
+//         console.log(e);
+        
+//     }
+// }
+// let e = 20
+// c()
+
+function a(){
+    let b  = 10
+}
+
+console.log(b);
