dbf5c1c1471d819a6481f9137147a12b769c11c9
Krish Sharma <krishsharma3355@gmail.com>
2026-09-15 21:37:51 +0530

    closures with the var scope

--- closures/closure.js
+++ closures/closure.js
@@ -1,13 +1,36 @@
 // set timeout closures
 
-function x(){
-    let a = 1
-    setTimeout(function(){
-        console.log(a)
-    }, 1000)
+// function x(){
+//     let a = 1
+//     setTimeout(function(){
+//         console.log(a)
+//     }, 1000)
+// }
+// x()
+
+
+// settimout run after the code in the same function
+
+// function y(){
+//     let a = 1
+//     setTimeout(function(){
+//         console.log(a);
+        
+//     }, 2000)
+//     console.log("hello world");
+    
+// }
+// y()
+
+
+function y(){
+    for(var i = 1; i<= 5; i++){
+        setTimeout(function(){
+            console.log(i);
+            
+        }, i * 1000)
+    }
+    console.log("hello")
 }
-x()
-
-
-
+y() // this print  6 6 6 6 6
 
