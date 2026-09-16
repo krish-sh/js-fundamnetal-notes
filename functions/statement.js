a542a3ce6b77aa5da3055908c95d4864ec299a3c
Krish Sharma <krishsharma3355@gmail.com>
2026-09-16 20:56:15 +0530

    First Class function

--- functions/statement.js
+++ functions/statement.js
@@ -1,41 +1,44 @@
 // Fucntion statement
 
-function greet(){
-    console.log("its a function statment");
-    
+function greet() {
+  console.log("its a function statment");
 }
 // greet()
 
 // Function expression
 
-let b = function(){
-    console.log("function expression");
-    
-}
+let b = function () {
+  console.log("function expression");
+};
 // b()
 
 // function decleration
 
-function sum(a,b){
-    return a+b
+function sum(a, b) {
+  return a + b;
 }
 // console.log(sum(1, 2));
 
-
 // Anonymous function
 
-let c = function(){
-    console.log("Anonymous function");
-    
-}
+let c = function () {
+  console.log("Anonymous function");
+};
 // c()
 
 // Named function expression
 
-let d = function xyz(){
-    console.log("Name function expression");
-    console.log(xyz);
-    
-    
+let d = function xyz() {
+  console.log("Name function expression");
+  console.log(xyz);
+};
+// d()
+
+// First class function
+
+function firstClass() {
+  return function xyz() {
+    console.log("First class function");
+  };
 }
-d()
+firstClass()()
