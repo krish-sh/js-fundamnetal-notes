93329c020438fdbdc9a42a181e5d05890c8ddf90
Krish Sharma <krishsharma3355@gmail.com>
2026-09-20 11:55:41 +0530

    reduce real database question

--- Functional-Programming/reduce-function.js
+++ Functional-Programming/reduce-function.js
@@ -1,16 +1,34 @@
-
-let nums = [6, 1, 4, 2, 5];
+// let nums = [6, 1, 4, 2, 5];
 
 // let output = nums.reduce((acc,curr) => acc += curr)
 
 // console.log(output);
 
-let max = nums.reduce((acc, curr) => {
-    
-    if(acc <= curr){
-        acc = curr
-    }
-    return acc
-})
-console.log(max);
+// let max = nums.reduce((acc, curr) => {
+
+//     if(acc <= curr){
+//         acc = curr
+//     }
+//     return acc
+// })
+// console.log(max);
+
+const users = [
+  { firstName: "akshay", lastName: "saini", Age: 26 },
+  { firstName: "donald", lastName: "trump", Age: 75 },
+  { firstName: "elon", lastName: "musk", Age: 50 },
+  { firstName: "deepika", lastName: "padukone", Age: 26 },
+];
+
+let output = users.reduce((acc, curr) => {
+  if (acc[curr.Age]) {
+    acc[curr.Age] += 1;
+  } else {
+    acc[curr.Age] = 1;
+  }
+
+   return acc
+}, {});
+
+console.log(output);
 
