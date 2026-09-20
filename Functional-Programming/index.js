b5ad60516e25fb4cb04a5a9c4bc780692ab289c4
Krish Sharma <krishsharma3355@gmail.com>
2026-09-20 10:18:24 +0530

    higher-order method

--- Functional-Programming/index.js
+++ Functional-Programming/index.js
@@ -34,3 +34,31 @@
 
 // console.log(Diameter(radius));
 
+// this is the best method to implement where the 90 percent of code is same
+
+let radius = [3, 1, 2, 4];
+
+
+let area = function (radius){
+    return Math.floor(Math.PI * (radius * radius))
+}
+
+let circumferrence = function (radius){
+    return Math.floor(Math.PI * radius)
+}
+
+let diameter = function (radius){
+    return Math.floor(2 * radius)
+}
+
+let calculate = function (radius, logic){
+    let output = []
+    for(let i = 0; i< radius.length; i++){
+        output.push(logic(radius[i]))
+    }
+    return output
+}
+
+console.log(calculate(radius, area));
+console.log(calculate(radius, circumferrence));
+console.log(calculate(radius, diameter));
