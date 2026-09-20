06c1f862f5edfbc15e91066b201824a7ce7f7a7f
Krish Sharma <krishsharma3355@gmail.com>
2026-09-20 10:17:23 +0530

    functional-Programming generic order

---
+++ Functional-Programming/index.js
@@ -0,0 +1,36 @@
+// this is the generic method
+// let radius = [3, 1, 2, 4];
+
+// let area = function (radius) {
+//   let output = [];
+
+//   for (let i = 0; i < radius.length; i++) {
+//     output.push(Math.floor(Math.PI * (radius[i] * radius[i])));
+//   }
+//   return output;
+// };
+
+// console.log(area(radius));
+
+// let circumferrence = function (radius) {
+//   let output = [];
+
+//   for (let i = 0; i < radius.length; i++) {
+//     output.push(Math.floor(2 * Math.PI * radius[i]));
+//   }
+//   return output;
+// };
+
+// console.log(circumferrence(radius));
+
+// let Diameter = function (radius) {
+//   let output = [];
+
+//   for (let i = 0; i < radius.length; i++) {
+//     output.push(Math.floor(2 * radius[i]));
+//   }
+//   return output;
+// };
+
+// console.log(Diameter(radius));
+
