3f7b7bd4128265c393481e5244e37eb6c083c522
Krish Sharma <krishsharma3355@gmail.com>
2026-09-22 19:30:12 +0530

    Practice promise chain

--- promises/creating-promise.js
+++ promises/creating-promise.js
@@ -2,15 +2,21 @@ let cart = ["pant", "shoes", "shirt"];
 
 // consumer part
 
+// Create order
 createOrder(cart)
   .then((orderId) => {
     console.log(orderId);
     return orderId;
-  }) // Promise chain
+  }) // Promise chain // procced to payment
   .then(function (orderId) {
     return proccedToPayment(orderId);
-  })
-  //Error handling
+  }) // show order summary
+  .then(function (items, orderId) {
+   return showOrderSummary(items);
+  }) // Update walllet
+  .then((wallet) => {
+    return updateWallet()
+  })//Error handling
   .catch(function (err) {
     console.log(err.message);
   });
@@ -47,8 +53,30 @@ function validateCart() {
 function proccedToPayment(orderId) {
   return new Promise((resolve, reject) => {
     resolve("Payment successfully");
-    console.log("done");
-    
-    
+    console.log("Payment successfully");
   });
+}
+
+function showOrderSummary() {
+  return new Promise((resolve, reject) => {
+    const items = cart;
+    if (items) {
+      console.log(items);
+      resolve(items);
+    }
+    let err = new Error("Error in order summary");
+    resolve(err);
+  });
+}
+
+ function updateWallet()  {
+    return new Promise((resolve, reject) => {
+        let bal = 1000
+        let orderItem = 300
+      const wallet =  bal - orderItem
+      bal = wallet
+      resolve(wallet)
+      console.log(wallet);
+      
+    })
 }
