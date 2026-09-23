b7c56b4a66769017c563ec06099ee8255ec20e16
Krish Sharma <krishsharma3355@gmail.com>
2026-09-23 21:21:15 +0530

    Promise-all

---
+++ promises/Pomise-api.js
@@ -0,0 +1,5 @@
+Promise.all()
+
+// its value work parallely and give the result at a same time. After its all Users Promises is completed 
+
+//  but if any user have error in the Promise api it imediatedly sent an error and no promise is successed then 
